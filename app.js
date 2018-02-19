7f605ddccc4097b0bd8645e7ff45e28241a488d1
Altamash <altamash.ansari@raweng.com>
2018-02-19 18:45:21 +0530

    1 add dev config

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltd470ad1e7ce19b96')
+var app = Built.App('blt7ff1904c6d3d9219')
 // .setHost('localhost')
 // .setPort(8000)
-.setHost("test-stag-api.built.io")
+.setHost("dev-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('bltd6b6eedeff10f9b0')
+.setMasterKey('blta49594c249a48599')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'altamash',
